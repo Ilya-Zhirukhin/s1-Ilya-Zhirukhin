48b6ef442aa49af6ffb68c6c0fbfdeea4c251d86
Ilya <perk.1@mail.ru>
2023-05-23 13:28:48 +0300

    __init__.py

---
+++ Tema/__init__.py
@@ -0,0 +1,40 @@
+from flask import Flask, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_socketio import SocketIO
+from os.path import join, dirname, realpath
+from flask import Flask, redirect, url_for, render_template
+from flask_dance.contrib.github import make_github_blueprint, github
+
+import eventlet
+
+eventlet.monkey_patch()
+
+app = Flask(__name__, template_folder="templates")
+app.config['SECRET_KEY'] = 'R\x1a^-\x14?mq\x1ce\xf7\xefi\xb7\x0e\xa0\x02\x0c\xd6-$\x033\xc4'
+app.config['IMAGE_UPLOADS'] = join(dirname(realpath(__file__)), 'static/imgs/')
+app.config['FILE_UPLOADS'] = join(dirname(realpath(__file__)), 'static/submissions/')
+app.config['UPLOAD_FOLDER'] = join(dirname(realpath(__file__)), 'static/upload_user')
+
+#
+# app.config['GITHUB_CLIENT_ID'] = 'fcea9d2bf64321447a57'
+# app.config['GITHUB_CLIENT_SECRET'] = '64e2c48ba1b1c3f9c0dfde7417a96014bb544113'
+
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.sqlite3'
+db = SQLAlchemy(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+# modules import
+from classroom_manager.routes import *
+from classroom_manager.models import User
+from classroom_manager.network import *
+
+with app.app_context():
+    db.create_all()
